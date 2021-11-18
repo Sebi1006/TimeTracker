80248ae72224ed947e7dcf7f4a63bfd51601cfff
Sebi1006 <sebi.reinbold@gmail.com>
2021-11-18 15:40:21 +0100

    Implemented tag page

---
+++ src/components/tag/tag-list-toolbar.js
@@ -0,0 +1,106 @@
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions
+} from '@mui/material';
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+export const TagListToolbar = (props) => {
+  const [dialog, setDialog] = useState(false);
+
+  const [values, setValues] = useState({
+    id: '',
+    name: ''
+  });
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      name: event.target.value,
+      id: uuid()
+    });
+  };
+
+  const openDialog = () => {
+    setDialog(true);
+  };
+
+  const closeDialog = () => {
+    setDialog(false);
+    setValues({
+      ...values,
+      name: ''
+    });
+  };
+
+  return (
+    <Box {...props}>
+      <Box
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexWrap: 'wrap',
+          m: -1
+        }}
+      >
+        <Typography
+          sx={{ m: 1 }}
+          variant="h4"
+        >
+          Tags
+        </Typography>
+        <Box sx={{ m: 1 }}>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={openDialog}
+          >
+            Add Tag
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ mt: 3 }}>
+      </Box>
+      <Dialog onClose={closeDialog} open={dialog}>
+        <DialogTitle>
+          Add Tag
+        </DialogTitle>
+        <DialogContent>
+          <Typography marginBottom={1}>
+            Enter the tag name you want to create:
+          </Typography>
+          <TextField
+            fullWidth
+            placeholder="Tag Name"
+            name="tag"
+            value={values.name}
+            onChange={handleChange}
+            margin={'dense'}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDialog}>
+            Cancel
+          </Button>
+          <Button
+            onClick={function () {
+              props.addTag(values);
+              closeDialog();
+            }}
+            autoFocus
+            disabled={values.name === ''}
+          >
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
+};
--- src/pages/projects-tags.js
+++ src/pages/projects-tags.js
@@ -1,15 +1,19 @@
 import Head from 'next/head';
 import { Box, Container, Grid, Pagination } from '@mui/material';
 import { projects } from '../__mocks__/projects';
+import { tags } from '../__mocks__/tags';
 import { ProjectListToolbar } from '../components/project/project-list-toolbar';
 import { ProjectCard } from '../components/project/project-card';
 import { DashboardLayout } from '../components/dashboard-layout';
 import { useState } from 'react';
+import { TagListToolbar } from '../components/tag/tag-list-toolbar';
+import { TagChip } from '../components/tag/tag-chip';
 
 const Projects = () => {
   const MAX_PROJECTS_PER_PAGE = 9;
   const [page, setPage] = useState(1);
   const [project, setProject] = useState(projects);
+  const [tag, setTag] = useState(tags);
 
   const handleChange = (event, value) => {
     setPage(value);
@@ -19,11 +23,15 @@ const Projects = () => {
     setProject(project => [...project, data]);
   };
 
+  const addTag = (data) => {
+    setTag(tag => [...tag, data]);
+  };
+
   return (
     <>
       <Head>
         <title>
-          Projects | Time Tracker
+          Projects & Tags | Time Tracker
         </title>
       </Head>
       <Box
@@ -74,6 +82,34 @@ const Projects = () => {
               />
             </Box>
           }
+        </Container>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8
+        }}
+      >
+        <Container maxWidth={false}>
+          <TagListToolbar addTag={addTag}/>
+          <Box sx={{ pt: 3 }}>
+            <Grid
+              container
+              spacing={3}
+            >
+              {tag
+                .map((tag) => (
+                  <Grid
+                    item
+                    key={tag.id}
+                    height="8vh"
+                  >
+                    <TagChip tag={tag}/>
+                  </Grid>
+                ))}
+            </Grid>
+          </Box>
         </Container>
       </Box>
     </>
